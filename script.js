835a051a4ac30f03cd5e70395ceef4a3af88884d
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-11-17 21:05:55 +0100

    script

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
+Document.lo
 const cardScroll = document.querySelector('.card-scroll');
-const cards = document.querySelectorAll('.card');  
+const cards = document.querySelectorAll('.card');
 
 cardScroll.addEventListener('scroll', () => {
-  cards.forEach(card => {  
-    const rect = card.getBoundingClientRect();  
+  cards.forEach(card => {
+    const rect = card.getBoundingClientRect();
     const scrollRect = cardScroll.getBoundingClientRect();
 
-            // Check if the card is partially out of view  
-            const isVisible = rect.right > scrollRect.left && rect.left < scrollRect.right;  
-
-            // If not visible, apply the blurry class  
-            if (!isVisible) {  
-                card.classList.add('blurry');  
-            } else {  
-                card.classList.remove('blurry');  
-            }  
-        });  
-    });  
-</script>
+    const isVisible = rect.right > scrollRect.left && rect.left < scrollRect.right;
+    if (!isVisible) card.classList.add('blurry');  
+    else card.classList.remove('blurry');  
+  });
+});

7ed584d4782877f572238332d1fa276669939df0
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-11-21 17:52:22 +0100

    script

--- script.js
+++ script.js
@@ -1,15 +1,48 @@
 document.addEventListener('DOMContentLoaded', () => {
+  fetch('https://api.github.com/users/RealTonyBlaq/repos')
+    .then(response => {
+      if (!response.ok) alert('Network connection error');
+      else return response.json();
+    })
+    .then(data => {
+      const projects = document.getElementsByClassName('projects');
+      for (const repo of data) {
+        const repoName = repo.name;
+        const description = repo.description;
+
+        const card = document.createElement('div');
+        card.className = 'card';
+
+        const img = document.createElement('img');
+        img.src = 'https://avatars.githubusercontent.com/u/132378140?v=4';
+        img.alt = `${repoName} image`;
+
+        const h3 = document.createElement('h3');
+        h3.textContent = repoName;
+
+        const p = document.createElement('p');
+        p.textContent = description;
+
+        card.appendChild(img);
+        card.appendChild(h3);
+        card.appendChild(p);
+
+        projects.appendChild(card);
+      }
+    })
+    .catch(err => alert(err.message));
+
   const cardScroll = document.querySelector('.scroller');
   const cards = document.querySelectorAll('.card');
 
   cardScroll.addEventListener('scroll', () => {
     cards.forEach(card => {
-    const rect = card.getBoundingClientRect();
-    const scrollRect = cardScroll.getBoundingClientRect();
+      const rect = card.getBoundingClientRect();
+      const scrollRect = cardScroll.getBoundingClientRect();
 
-    const isVisible = rect.right > scrollRect.left && rect.left < scrollRect.right;
-    if (!isVisible) card.classList.add('blurry');  
-    else card.classList.remove('blurry');  
+      const isVisible = rect.right > scrollRect.left && rect.left < scrollRect.right;
+      if (!isVisible) card.classList.add('blurry');
+      else card.classList.remove('blurry');
     });
   });
 });

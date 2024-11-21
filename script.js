a29baec2a3bb1303167d91dcf28f5fec13596ce8
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-11-21 11:15:18 +0100

    script

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
-});
+  const cardScroll = document.querySelector('.scroller');
+  const cards = document.querySelectorAll('.card');
 
-const cardScroll = document.querySelector('.scroller');
-const cards = document.querySelectorAll('.card');
-
-cardScroll.addEventListener('scroll', () => {
-  cards.forEach(card => {
+  cardScroll.addEventListener('scroll', () => {
+    cards.forEach(card => {
     const rect = card.getBoundingClientRect();
     const scrollRect = cardScroll.getBoundingClientRect();
 
     const isVisible = rect.right > scrollRect.left && rect.left < scrollRect.right;
     if (!isVisible) card.classList.add('blurry');  
     else card.classList.remove('blurry');  
+    });
   });
+
+  
 });

fc0d85e65326d19064cae46385e7b1ff9223415d
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-11-21 17:49:18 +0100

    script

--- script.js
+++ script.js
@@ -12,37 +12,4 @@ document.addEventListener('DOMContentLoaded', () => {
     else card.classList.remove('blurry');  
     });
   });
-
-  fetch('https://api.github.com/users/RealTonyBlaq/repos')
-    .then(response => {
-      if (!response.ok) alert('Network connection error');
-      else return response.json();
-    })
-    .then(data => {
-      const projects = document.getElementsByClassName('projects');
-      for (const repo in data) {
-        const repoName = repo.name;
-        const description = repo.description;
-
-        const card = document.createElement('div');
-        card.className = 'card';
-
-        const img = document.createElement('img');
-        img.src = 'https://avatars.githubusercontent.com/u/132378140?v=4';
-        img.alt = `${repoName} image`;
-
-        const h3 = document.createElement('h3');
-        h3.textContent = repoName;
-
-        const p = document.createElement('p');
-        p.textContent = description;
-
-        card.appendChild(img);
-        card.appendChild(h3);
-        card.appendChild(p);
-
-        projects.appendChild(card);
-      }
-    })
-    .catch()
 });

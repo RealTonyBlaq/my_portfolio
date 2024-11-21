adf0dab07a2c4f63e1c3401fdedab40c0e54b14d
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-11-21 11:24:26 +0100

    script

--- script.js
+++ script.js
@@ -14,5 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   fetch('https://api.github.com/users/RealTonyBlaq/repos')
-    .then(response => )
+    .then(response => {
+      if (!response.ok) 
+    })
 });
